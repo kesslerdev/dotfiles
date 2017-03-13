01fcd85e1eb702b92cb5e4d33c57089f82b6048b
Kessler dev <kessler.dev@gmail.com>
2017-03-13 14:04:33 +0100

    update sf commands

--- alias/sf.sh
+++ alias/sf.sh
@@ -6,12 +6,16 @@ sf() {
         command bin/console cache:clear --env=test
     elif [[ $@ == "b" ]]; then
 	    command bin/behat
+    elif [[ $@ == "mock" ]]; then
+	    command bin/sf3_restart_mock_servers
+    elif [[ $@ == "mockb" ]]; then
+	    sf mock && sf b
     elif [[ $@ == "br" ]]; then
 	    command bin/behat --rerun
     elif [[ $@ == "ccb" ]]; then
-	    command bin/console cache:clear --env=test && bin/behat
+	    sf cc && sf b
     elif [[ $@ == "ccbr" ]]; then
-	    command bin/console cache:clear --env=test && bin/behat --rerun
+	    sf cc && sf br
     else
         command echo "unknown operation" "\"$@\""
     fi
