5d6da50742194bc8ba0793f72c1116e8a4dc8af8
Kessler dev <kessler.dev@gmail.com>
2017-04-19 16:03:01 +0200

    add pgclient alias

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 One line install
 
 `git clone https://github.com/kesslerdev/dotfiles.git ~/dotfiles && cd ~/dotfiles && chmod +x makesymlinks.sh && ./makesymlinks.sh`
+
+## TODO
+
+- [ ] at install on copy old dotfiles check if file exist to avoid "mv: cannot stat ‘/home/skimia/.bashrc’: No such file or directory"
--- alias/main.sh
+++ alias/main.sh
@@ -4,3 +4,16 @@
 alias zshreload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 
 alias reset-wifi='echo "stop networkmanager..." && sudo service network-manager stop && sleep 3 && echo "clear state..." && sudo rm /var/lib/NetworkManager/NetworkManager.state && sleep 3 && echo "start networkmanager..." && sudo service network-manager start'
+
+pg-client() {
+    if [[ $1 == "import" ]]; then
+        command psql -h db -U root -W -d $2 -f $3
+    elif [[ $1 == "export" ]]; then
+
+    else
+        if [[ $1 != "" ]]; then
+            command echo "unknown operation" "\"$1\" => defaults to pg-client"
+        fi
+        command psql -h db -U root ${@:1}
+    fi
+}
